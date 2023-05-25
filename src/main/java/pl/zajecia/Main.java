8a572bf9f855dbb9f3154ed0cd5c19b2914575e0
TomaszKulczycki <tomasz.kulczycki@rebit.pl>
2023-05-25 17:19:10 +0200

    minor main change

--- src/main/java/pl/zajecia/Main.java
+++ src/main/java/pl/zajecia/Main.java
@@ -2,6 +2,6 @@ package pl.zajecia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 }
